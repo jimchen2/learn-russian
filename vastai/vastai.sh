235cb6b3d5e468af0cb6c71b7154f7c9fdc346e6
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2024-08-25 05:51:24 +0800

    Update vastai.sh

--- vastai/vastai.sh
+++ vastai/vastai.sh
@@ -1,5 +1,6 @@
 conda install -c conda-forge ffmpeg=6 cudatoolkit
 pip install yt-dlp boto3 uuid python-dotenv transformers openai-whisper sentencepiece sacremoses
+apt install vim
 
 
 cd ~ && git clone https://github.com/jimchen2/whisper-video-s3 && cd ~/whisper-video-s3 
