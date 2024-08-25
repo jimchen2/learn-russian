270effb8c28a5d57f417d968e3290c089ccae0b4
Jim Chen <jimchen4214@gmail.com>
2024-08-25 14:04:29 +0800

    Fix YouTube Raw

--- youtube/download_raw.sh
+++ youtube/download_raw.sh
@@ -1,9 +1,20 @@
 mkdir -p temp && cd temp
 
-# Download videos matching the criteria
+# # Download videos matching the criteria
+# yt-dlp -j --flat-playlist "https://www.youtube.com/openai" |
+#     jq -r 'select(.view_count != null and .view_count > 50000 and .duration != null and .duration > 300) | "\(.view_count) https://youtu.be/\(.id)"' |
+#     sort -rn | cut -d' ' -f2- | xargs -I {} yt-dlp {}
+
+
+
+
+# 720p
 yt-dlp -j --flat-playlist "https://www.youtube.com/openai" |
     jq -r 'select(.view_count != null and .view_count > 50000 and .duration != null and .duration > 300) | "\(.view_count) https://youtu.be/\(.id)"' |
-    sort -rn | cut -d' ' -f2- | xargs -I {} yt-dlp {}
+    sort -rn | cut -d' ' -f2- | xargs -I {} yt-dlp -f 'bestvideo[height<=720]+bestaudio/best[height<=720]/best' {}
+
+
+
 
 # Use rclone to upload the directory
 rclone copy . r2:bucket/prefix/
