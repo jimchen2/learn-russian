1e55968e080ce24dc3b207dcdb6b34fa3129eed2
Jim Chen <jimchen4214@gmail.com>
2024-08-25 22:34:18 +0800

    Fix YouTube Raw

--- vastai/vastai.sh
+++ vastai/vastai.sh
@@ -4,6 +4,12 @@ sudo apt-get install -y vim
 cd ~ && git clone https://github.com/jimchen2/whisper-video-s3 && cd ~/whisper-video-s3 
 
 
+# Do a Dummy Run to Download Weights(multithreading downloading weights would be disaster)
+
+cat << EOF > video_urls.txt
+https://peertube.jimchen.me/w/iSRQZzTpSs2dLtsdMV9Q3d
+EOF
+python run.py -t 1
 
 # use this to stop errors
 # export MKL_THREADING_LAYER=GNU
