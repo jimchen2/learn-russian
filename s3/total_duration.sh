761202300d513bcac1963962a0f3987b10bfb98c
Jim Chen <jimchen4214@gmail.com>
2024-08-26 08:27:57 +0800

    Updates

--- s3/total_duration.sh
+++ s3/total_duration.sh
@@ -6,13 +6,16 @@
 
 
 
-BASE_URL="https://pub-b64e8549cb8d40a082e1f6f1afd4cb58.r2.dev"
-TOTAL_DURATION=0
+# BASE_URL="https://pub-b64e8549cb8d40a082e1f6f1afd4cb58.r2.dev"
+# TOTAL_DURATION=0
 
-rclone ls r2:jimchen4214-public | while read -r size object; do
-    duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "${BASE_URL}/${object}")
-    printf "%.6f - %s\n" "$duration" "$object"
-    TOTAL_DURATION=$(bc <<<"$TOTAL_DURATION + $duration")
-done
+# rclone ls r2:jimchen4214-public | while read -r size object; do
+#     duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "${BASE_URL}/${object}")
+#     printf "%.6f - %s\n" "$duration" "$object"
+#     TOTAL_DURATION=$(bc <<<"$TOTAL_DURATION + $duration")
+# done
 
-echo "Total duration: ${TOTAL_DURATION} seconds"
+# echo "Total duration: ${TOTAL_DURATION} seconds"
+
+
+# file is buggy can't use
--- youtube/download_raw.sh
+++ youtube/download_raw.sh
@@ -5,15 +5,10 @@ mkdir -p temp && cd temp
 #     jq -r 'select(.view_count != null and .view_count > 50000 and .duration != null and .duration > 300) | "\(.view_count) https://youtu.be/\(.id)"' |
 #     sort -rn | cut -d' ' -f2- | xargs -I {} yt-dlp {}
 
-
-
-
 # 720p
 yt-dlp -j --flat-playlist "https://www.youtube.com/openai" |
     jq -r 'select(.view_count != null and .view_count > 50000 and .duration != null and .duration > 300) | "\(.view_count) https://youtu.be/\(.id)"' |
-    sort -rn | cut -d' ' -f2- | xargs -I {} yt-dlp -f 'bestvideo[height<=720][ext=webm]+bestaudio[ext=webm]/best[height<=720][ext=webm]/best[ext=webm]' {}
-
-
+    sort -rn | cut -d' ' -f2- | xargs -I {} yt-dlp -f 'bestvideo[height<=720][ext=webm]+bestaudio[ext=webm]/bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a]/best[height<=720][ext=webm]/best[height<=720][ext=mp4]/best[ext=webm]/best[ext=mp4]' {}
 
 # Use rclone to upload the directory
 rclone copy . r2:bucket/prefix/
